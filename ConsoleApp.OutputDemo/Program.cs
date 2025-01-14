e4c24173ceaf676c3b473cdd6fadebf943d2dd1c
Marcelo Rodriguez <reyesmarcel24@gmail.com>
2025-01-14 14:50:46 -0600

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//WriteLine is a method that takes a block of text and print it on the screen.
+Console.WriteLine("Hello, World! My name is Marcelo Rodriguez");
+Console.WriteLine("I am from Nicaragua!");
+Console.WriteLine("Anything you want to say");
+
+//Write is a method that takes a block of text and print it on the screen.
+//It does not break the line after it prints the text.
+Console.Write("Line 1");
+Console.Write("Line 2");
+
+
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Different datatypes
+/*
+    text - string
+    integers - int
+    decimal - double, float, decimal
+    logical - bool
+ */
+
+string name = "Marcelo Rodriguez";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name); //String concatenation
+Console.WriteLine($"They call me {name}"); //String interpolation
+Console.WriteLine("I was given the name {0}", name); //Formatted string
+
+int age = 21;
+int retirementYearsLeft = 44;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("My age is: " + age);
+Console.WriteLine("My retirement age is: " + retirementAge);
+
+bool isRetired = true;
+Console.WriteLine("Am I retired? " + isRetired);
