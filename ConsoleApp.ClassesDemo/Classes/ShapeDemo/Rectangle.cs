f79ab34917069058b991f53ad1ca397bac021e15
Marcelo Rodriguez <reyesmarcel24@gmail.com>
2025-02-12 16:29:14 -0600

    Explored OPP. Modified UserInputDemo

---
+++ ConsoleApp.ClassesDemo/Classes/ShapeDemo/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp.ClassesDemo.Interfaces;
+
+namespace ConsoleApp.ClassesDemo
+{
+    public class Rectangle : Polygon, I2DShape
+    {
+        public Rectangle(int width, int length)
+        {
+            Length = length;
+            Width = width;
+        }
+        public int Length { get; set; }
+        public override double Area()
+        {
+            return Length * Width;
+        }
+
+        public double Perimeter()
+        {
+            return (2 * Length) + (2 * Width);
+        }
+
+        public override string GetShapeName()
+        {
+            return base.GetShapeName();
+        }
+    }
+}
---
+++ ConsoleApp.ClassesDemo/Classes/ShapeDemo/Triangle.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp.ClassesDemo
+{
+    public class Triangle : Polygon
+    {
+        public Triangle(int height, int @base)
+        {
+            Height = height;
+            Base = @base;
+        }
+        public int Height { get; set; }
+        public int Base { get; set; }
+        public override double Area()
+        {
+            return (Height * Base) / 2;
+        }
+    }
+}
